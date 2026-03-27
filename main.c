3a2400d62fa09f6e7ed5261fd6c94db48640514f
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-03-27 01:12:57 -0300

    declarando as funções e definindo alguns macros

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#define HELP "Show all commands."
+#define SET_NEW_NAME "Set a new user name."
+#define CLEAR "Clear console."
+#define GET_CURRENT_USERNAME "Get Current User"
+
+int help();
+int ClearConsole();
+void SetNewName(char *Array);
+int GetUsername(char *Array);
+int ClearArray(char *Array);
+
 int main(){
     return 0;
 }
