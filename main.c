7b43ec266a740f709d1c5e83b824f5d4a527b726
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-03-27 01:16:16 -0300

    criando a lógica das funções

--- main.c
+++ main.c
@@ -12,3 +12,40 @@ int ClearArray(char *Array);
 int main(){
     return 0;
 }
+
+int help(){
+    printf("\n");
+    printf("HELP: %s\n", HELP);
+    printf("SET_NEW_NAME/snn: %s\n", SET_NEW_NAME);
+    printf("GET_CURRENT_USERNAME/snn: %s\n", GET_CURRENT_USERNAME);
+    printf("clear/cls: %s\n", CLEAR);
+    printf("\n");
+    return 0;
+}   
+
+void SetNewName(char *Array){
+    char Array[255];
+    scanf("%s\n", Array);
+    printf("\n");
+}
+
+int GetUsername(char *Array){
+
+    if (strcmp(Array, "\n") == 0 || strcmp(Array, "\0") == 0){
+        Array = "Default";
+    }
+
+    printf("Active user: [%s]", Array);
+}
+
+int ClearConsole(){
+    system("cls");
+}
+
+int ClearArray(char *Array){
+    for (int i = 0; i < sizeof(Array)/sizeof(Array[0]); i++){
+        Array[i] = '\0';
+    }
+
+    return 0;
+}
