d176104c16028a681995d64488bf14fa75fcbb2e
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-06-10 02:46:57 -0300

    Modularizando a parte de commandos, structs, e enums no arquivo Commands

---
+++ Commands.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Commands.h"
+#include "History.h"
+#include "User.h"
+
+static const char version[] = "v0.0.9";
+
+Command commands[] = {
+    {"help", "Show all commands.", HELP},
+    {"setuser", "Set a new user name.", SETUSER},
+    {"getuser", "Get Current User.", GETUSER},
+    {"clear", "Clear console.", CLEARCONSOLE},
+    {"cmdh", "Get all commands used.", GETHISTORY},
+    {"--version", "Get current terminal version", VERSION},
+    {"exit", "Close the terminal.", EXIT},
+};
+
+const int COMMANDSIZE = (sizeof(commands) / sizeof(commands[0]));
+
+enum Functions InputHandler(char* InputArray, int* count, char commandList[100][255]){
+    scanf("%254s", InputArray);
+    printf("\n");
+    AddNewCommandToHistory(count, commandList, InputArray);
+    for (int i = 0; i < COMMANDSIZE; i++){
+        if (strcmp(InputArray, commands[i].name) == 0){
+            return commands[i].func;
+        }
+    }
+
+    return -1;
+}
+
+int help(){
+    for (int i = 0; i < COMMANDSIZE; i++){
+        printf("%s: %s\n", commands[i].name, commands[i].description);
+    }
+    return 0;
+}
+
+void ClearConsole(){
+    system("cls");
+}
+
+void CurrentVersion(){
+    printf("\n[Author]: {Grim-Undead-Reaper}\n[Software]: {First_C_UI}\n[Version]: {%s}\n", version);
+}
---
+++ Commands.h
@@ -0,0 +1,29 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+enum Functions {
+  HELP,
+  SETUSER,
+  GETUSER,
+  CLEARCONSOLE,
+  GETHISTORY,
+  VERSION,
+  EXIT
+};
+
+typedef struct {
+  char *name;
+  char *description;
+  enum Functions func;
+} Command;
+
+extern Command commands[];
+extern const int COMMANDSIZE;
+
+enum Functions InputHandler(char* InputArray, int* count, char commandList[100][255]);
+
+int help();
+void ClearConsole();
+void CurrentVersion();
+
+#endif
