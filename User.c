2d8c196efe861eea49d8c991b68b75fba14a12c4
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-06-10 02:51:08 -0300

    Modularizando as funções referentes ao usuário

---
+++ User.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void SetNewName(char *Array){
+    printf("\n");
+    printf("New username: ");
+    scanf("%254s", Array);
+    printf("\nName added Succeessify!\n");
+}
+
+void GetUsername(char *Array){
+
+    if (strcmp(Array, "?") == 0){
+        strcpy(Array, "Default");
+    }
+
+    printf("\nActive user: [%s]\n", Array);
+}
---
+++ User.h
@@ -0,0 +1,7 @@
+#ifndef USER_H
+#define USER_H
+
+void SetNewName(char *Array);
+void GetUsername(char *Array);
+
+#endif
