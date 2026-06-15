4a3aa5e63490f2af15f14ebc3e827186cb9ff098
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-06-15 04:53:33 -0300

    Pequeno reajustes no asterisco dos ponteiros kkkkkkkkk commit idiota eu sei

--- User.c
+++ User.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-void SetNewName(char *Array){
+void SetNewName(char* Array){
     printf("\n");
     printf("New username: ");
     scanf("%254s", Array);
     printf("\nName added Succeessify!\n");
 }
 
-void GetUsername(char *Array){
+void GetUsername(char* Array){
 
     if (strcmp(Array, "?") == 0){
         strcpy(Array, "Default");
--- main.c
+++ main.c
@@ -5,3 +5,5 @@ int main()
     CreateTerminal();
     return 0;
 }
+
+// cmd for compile the project: gcc *.c -Wall -Wextra -pedantic -o Terminal
